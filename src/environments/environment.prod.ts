03438d19c4631b5121e277732afa25df4d39a14f
Yuliasik <yulichka.dudij@gmail.com>
2023-01-22 18:19:19 +0200

    [OPS-1]: Fix release bugs.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  api_url: 'https://time-manager-api-2v5u4sj6sa-uc.a.run.app'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: false
+  production: false,
+  api_url: 'http://localhost:8080'
 };
