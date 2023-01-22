5f9eddea912925f54097124054e794c439381962
Yuliasik <yulichka.dudij@gmail.com>
2023-01-22 17:58:38 +0200

    [OPS-1]: Remove captcha.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,3 @@
 export const environment = {
   production: true,
-  recaptcha: {
-    siteKey: '6LcUNiAjAAAAAAz6BaSflQbNmWrsqC1-lyzG_ekg'
-  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,3 @@
 export const environment = {
-  production: false,
-  recaptcha: {
-    siteKey: '6LcUNiAjAAAAAAz6BaSflQbNmWrsqC1-lyzG_ekg'
-  }
+  production: false
 };
